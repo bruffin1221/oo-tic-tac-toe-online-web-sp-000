4d477ac071ef6bc7ea4737e5bf98803f6c529ced
Bruce Ruffin <bruffin1221@gmail.com>
2019-11-07 05:44:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,7 @@ def turn
   index=input_to_index(user_input)
   current_player
   if valid_move?(index)
-      @board[index] = current_player
+    move(index, current_player)
   else turn
   end
 end
