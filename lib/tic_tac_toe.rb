be0c5946a51cbfdcbb502be9831823631588719d
Bruce Ruffin <bruffin1221@gmail.com>
2019-11-07 05:24:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,7 +56,7 @@ def turn
   user_input=gets.chomp
   index=input_to_index(user_input)
   if valid_move?(index)
-    move(index, cuurent_player)
+    move(index, current_player)
   else turn
   end
 end
