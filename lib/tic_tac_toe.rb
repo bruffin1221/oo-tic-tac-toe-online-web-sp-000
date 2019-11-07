0561e62519d336af9b1772e5590d81a07fef58a0
Bruce Ruffin <bruffin1221@gmail.com>
2019-11-07 03:51:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,9 +55,9 @@ def turn
   puts "Please enter 1-9:"
   user_input=gets.chomp
   index=input_to_index(user_input)
+  current_player
   if valid_move?(index)
-    move
-    current_player
+    index=input_to_index(user_input)
   end
 end
 
